3ebaa43b273bc366ebb4964f443242267ea0a51c
Tomi Belan <belan@google.com>
2021-05-17 22:51:12 +0200

    tomi burrow2 magic patch: forward 22,5000-5999, redirect 80 to unix, fix #5021

--- wgengine/netstack/netstack.go
+++ wgengine/netstack/netstack.go
@@ -56,6 +56,8 @@ const debugPackets = false
 
 var debugNetstack = envknob.Bool("TS_DEBUG_NETSTACK")
 
+var port80env = os.Getenv("TOMI_TS_PORT80_SOCK")
+
 var (
 	magicDNSIP   = tsaddr.TailscaleServiceIP()
 	magicDNSIPv6 = tsaddr.TailscaleServiceIPv6()
@@ -748,6 +750,22 @@ func (ns *Impl) acceptTCP(r *tcp.ForwarderRequest) {
 		r.Complete(true) // sends a RST
 		return
 	}
+	if !isTailscaleIP {
+		ns.logf("[tomi] isTailscaleIP is false, what?! %v", dialIP)
+		r.Complete(true)
+		return
+	}
+	lp := reqDetails.LocalPort
+	if !(lp == 22 || lp == 80 || (lp >= 5000 && lp < 6000)) {
+		ns.logf("[tomi] forbidden port %v", reqDetails.LocalPort)
+		r.Complete(true)
+		return
+	}
+	if lp == 80 && port80env == "" {
+		ns.logf("[tomi] port 80 sock not configured")
+		r.Complete(true)
+		return
+	}
 	r.Complete(false)
 
 	// SetKeepAlive so that idle connections to peers that have forgotten about
@@ -763,7 +781,7 @@ func (ns *Impl) acceptTCP(r *tcp.ForwarderRequest) {
 	// as lingering connections to fork style daemons. On the other side of the
 	// fence, the long duration timers are low impact values for battery powered
 	// peers.
-	ep.SocketOptions().SetKeepAlive(true)
+//ep.SocketOptions().SetKeepAlive(true) // EDITED: fix #5021
 
 	// The ForwarderRequest.CreateEndpoint above asynchronously
 	// starts the TCP handshake. Note that the gonet.TCPConn
@@ -815,6 +833,11 @@ func (ns *Impl) acceptTCP(r *tcp.ForwarderRequest) {
 func (ns *Impl) forwardTCP(client *gonet.TCPConn, clientRemoteIP netaddr.IP, wq *waiter.Queue, dialAddr netaddr.IPPort) {
 	defer client.Close()
 	dialAddrStr := dialAddr.String()
+	network := "tcp"
+	if dialAddr.Port() == 80 {
+		network = "unix"
+		dialAddrStr = port80env
+	}
 	if debugNetstack {
 		ns.logf("[v2] netstack: forwarding incoming connection to %s", dialAddrStr)
 	}
@@ -839,16 +862,18 @@ func (ns *Impl) forwardTCP(client *gonet.TCPConn, clientRemoteIP netaddr.IP, wq 
 		cancel()
 	}()
 	var stdDialer net.Dialer
-	server, err := stdDialer.DialContext(ctx, "tcp", dialAddrStr)
+	server, err := stdDialer.DialContext(ctx, network, dialAddrStr)
 	if err != nil {
 		ns.logf("netstack: could not connect to local server at %s: %v", dialAddrStr, err)
 		return
 	}
 	defer server.Close()
+	if network == "tcp" {
 	backendLocalAddr := server.LocalAddr().(*net.TCPAddr)
 	backendLocalIPPort, _ := netaddr.FromStdAddr(backendLocalAddr.IP, backendLocalAddr.Port, backendLocalAddr.Zone)
 	ns.e.RegisterIPPortIdentity(backendLocalIPPort, clientRemoteIP)
 	defer ns.e.UnregisterIPPortIdentity(backendLocalIPPort)
+	}
 	connClosed := make(chan error, 2)
 	go func() {
 		_, err := io.Copy(server, client)
@@ -866,6 +891,7 @@ func (ns *Impl) forwardTCP(client *gonet.TCPConn, clientRemoteIP netaddr.IP, wq 
 }
 
 func (ns *Impl) acceptUDP(r *udp.ForwarderRequest) {
+	return
 	sess := r.ID()
 	if debugNetstack {
 		ns.logf("[v2] UDP ForwarderRequest: %v", stringifyTEI(sess))
